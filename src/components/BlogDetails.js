58f81a61e8c184fae06363523d188a50ef34d65d
ishanknijhawan <ishanknijhawan@gmail.com>
2022-04-01 15:31:24 +0530

    commit

---
+++ src/components/BlogDetails.js
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+
+const BlogDetails = () => {
+  const { id } = useParams();
+  const {
+    data: blog,
+    error,
+    isLoading,
+  } = useFetch(`http://localhost:8000/blogs/${id}`);
+  return (
+    <div className="blog-details">
+      {error && <div>{error}</div>}
+      {isLoading && <div>Loading...</div>}
+      {blog && (
+        <article>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+          <div>{blog.body}</div>
+        </article>
+      )}
+    </div>
+  );
+};
+
+export default BlogDetails;
